96a160d479a0853118deea99184b9a5d39faa4ac
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2026-04-17 17:59:09 +0000

    add weather data get

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
+import requests
+import json
+import dotenv
+import os
+from datetime import datetime
+
+
+# Load environment variables -- dont need this
+# dotenv.load_dotenv()
+# api_key = os.getenv("api_key")
+
+# Configuration for NWS API
+HEADERS = {
+    'User-Agent': '(myweatherapp.com, contact@email.com)'
+}
 
 class WeatherRecord:
-  def __init__(self, date, temperature, humidity,windSpeed,rainfall):
-    self.date = date
-    self.temperature = temperature
-    self.humidity = humidity
-    self.windSpeed = windSpeed
-    self.rainfall = rainfall
-    self.item_list = [date, temperature, humidity, windSpeed, rainfall]
-    self.item_map = {
-      "date": date,
-      "temperature": temperature,
-      "humidity": humidity,
-      "windSpeed": windSpeed,
-      "rainfall": rainfall
-    }   
+    def __init__(self, date, temperature, humidity, windSpeed, rainfall):
+        self.date = date
+        self.state = "IL"
+        self.temperature = temperature
+        self.humidity = humidity
+        self.windSpeed = windSpeed
+        self.rainfall = rainfall
+        
+        # storeing items in a list and a map for easy access
+        self.item_list = [date, temperature, humidity, windSpeed, rainfall]
+        self.item_map = {
+            "date": date,
+            "temperature": temperature,
+            "humidity": humidity,
+            "windSpeed": windSpeed,
+            "rainfall": rainfall
+        }   
 
-today = WeatherRecord("December 3rd", 76,56,98,2)
-print(today.item_list)
+        # API Logic
+        with open('weather_data.txt', 'r') as file:
+    content = file.read()
+    print(content)
